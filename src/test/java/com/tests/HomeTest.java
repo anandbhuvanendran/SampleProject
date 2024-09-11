92dad9f2d2aaa038f28446a5f6f7afdeb8657190
anand <anandbnair1208@gmail.com>
2024-09-11 06:26:32 +0530

    jenkins

---
+++ src/test/java/com/tests/HomeTest.java
@@ -0,0 +1,10 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class HomeTest {
+    @Test
+    public void testHomePage() {
+        System.out.println("HOme page test");
+    }
+}
---
+++ src/test/java/com/tests/LoginTest.java
@@ -0,0 +1,10 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void testLogin() {
+        System.out.println("Sample test");
+    }
+}
